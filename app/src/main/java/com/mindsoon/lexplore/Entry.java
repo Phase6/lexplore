d6b15a9b0f7ed5b7563bd87cece7faaa42a31fd6
Stephen Greenfield <stephengreenfield@gmail.com>
2016-04-17 22:21:13 -0400

    v1.1 supports new restful api
    major overhaul to the gears to support new restful api and better error
    handling

--- app/src/main/java/com/mindsoon/lexplore/Entry.java
+++ app/src/main/java/com/mindsoon/lexplore/Entry.java
@@ -6,113 +6,147 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Entry {
-    final String word;
-    private String dictionaryName;
-    private String slangName;
-    private String thesaurusName;
-    private String dictionaryContent;
-    private String slangContent;
-    private String thesaurusContent;
+    public String word;
+    public Content dictionary;
+    public Content synonym;
+    public Content slang;
+    private String italicsTag;
+    private String lineTag;
+    private String webLinkTag;
+    private String smallTag;
+    private String urbanDictionaryTag;
+    private String synonymTag;
+    private String noResultsTag;
 
     public Entry(String word) {
-        this.word = word;
+        this.word = word.replace("%", "%%").replace("/", "");
+        this.dictionary = null;
+        this.synonym = null;
+        this.slang = null;
     }
 
-    public String getWord() { return this.word; }
+    public boolean hasAllContent() {
+        return this.dictionary != null && this.synonym != null && this.slang != null;
+    }
 
-    public void insertContent(JSONObject definitionObject, Context context) {
-        String lineTag = context.getResources().getString(R.string.line_tag),
-                smallTag = context.getResources().getString(R.string.small_tag),
-                smallEndTag = context.getResources().getString(R.string.small_end_tag);
+    public boolean hasAnyContent() {
+        return this.dictionary != null || this.synonym != null || this.slang != null;
+    }
 
+    public void hydrateContentFromJson(JSONObject json, Verb verb, Context context) {
+        initializeHtmlStrings(context);
         try {
-            //dictionary content
-            this.dictionaryName = definitionObject.getJSONObject("dictionary").getString("name");
-            this.dictionaryContent = getDictionaryContent(context, this.dictionaryName, smallTag,
-                smallEndTag, lineTag, definitionObject.getJSONObject("dictionary").getString("link"),
-                definitionObject.getJSONObject("dictionary").getJSONArray("entries"));
-
-            //slang content
-            this.slangName = definitionObject.getJSONObject("slang").getString("name");
-            this.slangContent = getSlangContent(context, this.slangName, lineTag,
-                definitionObject.getJSONObject("slang").getString("link"),
-                definitionObject.getJSONObject("slang"));
-
-            //thesaurus content
-            this.thesaurusName = definitionObject.getJSONObject("thesaurus").getString("name");
-            this.thesaurusContent = getThesaurusContent(context, this.thesaurusName, smallTag,
-                smallEndTag, lineTag, definitionObject.getJSONObject("thesaurus").getString("link"),
-                definitionObject.getJSONObject("thesaurus").getJSONArray("entries"));
+            JSONObject lexplore = json.getJSONObject("lexplore");
+            if (verb == Verb.Dictionary && lexplore.has("dictionary")) {
+                this.dictionary = getDictionaryFromJson(lexplore.getJSONObject("dictionary"));
+            } else if (verb == Verb.Slang && lexplore.has("slang")) {
+                this.slang = getSlangFromJson(lexplore.getJSONObject("slang"));
+            } else if (verb == Verb.Synonym && lexplore.has("synonym")) {
+                this.synonym = getSynonymFromJson(lexplore.getJSONObject("synonym"));
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    private String getDictionaryContent(Context c, String name, String smallTag, String smallEndTag,
-                       String lineTag, String link, JSONArray entries) throws JSONException {
-        JSONArray defs;
-        String content = "";
+    private Content getDictionaryFromJson(JSONObject json) throws JSONException {
+
+        if (json.isNull("name") || json.isNull("link") || json.isNull("entries")) {
+            return noResults("definition", "");
+        }
+
+        Content content = new Content(json.getString("name"), "");
+        JSONArray entries = json.getJSONArray("entries");
+
         for (int i = 0; i < entries.length(); i++) {
-            content += smallTag + entries.getJSONObject(i).getString("firstUsage") + " (" +
-                    entries.getJSONObject(i).getString("partOfSpeech") + ")" +
-                    lineTag + smallEndTag;
-            defs = entries.getJSONObject(i).getJSONArray("definitions");
-            for (int j = 0; j < defs.length(); j++) {
-                content += " " + defs.getJSONObject(j).getString("def") + lineTag;
+            JSONObject entry = entries.getJSONObject(i);
+            if (entry.has("firstUsage") && entry.has("partOfSpeech") && entry.has("definitions")) {
+                content.content += String.format(smallTag,
+                        entry.getString("firstUsage") + " (" + entry.getString("partOfSpeech") + ")" + lineTag);
+                JSONArray defs = entry.getJSONArray("definitions");
+                for (int j = 0; j < defs.length(); j++) {
+                    JSONObject def = defs.getJSONObject(j);
+                    if (def.has("definition")) {
+                        content.content += " " + def.getString("definition");
+                    }
+                }
+                content.content += lineTag;
             }
         }
-        if (!content.equals(""))
-            return content.substring(0, content.length() - lineTag.length()) + linkToWeb(c, link);
-        else return noResults(c, name, link);
+        String webLink = json.getString("link");
+        content.content = content.content.substring(0, content.content.length() - lineTag.length()) + String.format(webLinkTag, webLink);
+        return content.content.equals("") ? noResults(content.name, webLink) : content;
     }
 
-    private String getSlangContent(Context c, String name, String lineTag, String link,
-                                   JSONObject slangObject) throws JSONException {
-        if (slangObject.getString("definition").length() > 0) {
-            return slangObject.getString("definition") + lineTag +
-                    "<i>" + slangObject.getString("example") + "</i>" + linkToWeb(c, link);
-        } else return noResults(c, name, link);
+    private Content getSlangFromJson(JSONObject json) throws JSONException {
+
+        if (json.isNull("name") || json.isNull("link") || json.isNull("definition") || json.isNull("example")) {
+            return noResults("slang", "");
+        }
+
+        String webLink = json.getString("link");
+        if (json.getString("definition").equals("")) {
+            return noResults(json.getString("name"), String.format(urbanDictionaryTag, word));
+        } else {
+            return new Content(json.getString("name"), json.getString("definition") +
+                                                       lineTag +
+                                                       String.format(italicsTag, json.getString("example")) +
+                                                       String.format(webLinkTag, webLink));
+        }
     }
 
-    private String getThesaurusContent(Context c, String name, String smallTag, String smallEndTag,
-                            String lineTag, String link, JSONArray entries) throws JSONException {
-        String content = "";
+    private Content getSynonymFromJson(JSONObject json) throws JSONException {
+
+        if (json.isNull("name") || json.isNull("link") || json.isNull("entries")) {
+            return noResults("synonym", "");
+        }
+
+        Content content = new Content(json.getString("name"), "");
+        String webLink = json.getString("link");
+        JSONArray entries = json.getJSONArray("entries");
+
         for (int i = 0; i < entries.length(); i++) {
-            content += smallTag + entries.getJSONObject(i).getString("partOfSpeech") +
-                    smallEndTag + entries.getJSONObject(i).getString("synonyms") + lineTag;
+            JSONObject entry = entries.getJSONObject(i);
+            if (entry.has("partOfSpeech") && entry.has("synonyms")) {
+                content.content += String.format(smallTag, entry.getString("partOfSpeech")) + " " +
+                        entry.getString("synonyms") +
+                        lineTag;
+            }
         }
-        if (!content.equals(""))
-            return content.substring(0, content.length() - lineTag.length()) + linkToWeb(c, link);
-        else return noResults(c, name, link);
+        content.content = content.content.substring(0, content.content.length() - lineTag.length()) + String.format(webLinkTag, webLink);
+        return (content.content.equals("")) ? noResults(json.getString("name"), String.format(synonymTag, word)) : content;
     }
 
-    private String noResults(Context c, String name, String link) {
-        String addDefinition = "",
-               urbanDictionaryOpenTag = c.getResources().getString(R.string.urban_dictionary_open_tag),
-               urbanDictionaryCloseTag = c.getResources().getString(R.string.urban_dictionary_close_tag),
-               wikisaurusOpenTag = c.getResources().getString(R.string.wikisaurus_open_tag),
-               wikisaurusCloseTag = c.getResources().getString(R.string.wikisaurus_close_tag),
-               noResultsOpenTag = c.getResources().getString(R.string.no_results_open_tag),
-               noResultsCloseTag = c.getResources().getString(R.string.no_results_close_tag);
+    private Content noResults(String verb, String link) {
+        return new Content("", String.format(noResultsTag + word, verb) + link);
+    }
 
-        if (name.equals("urban dictionary")) {
-            addDefinition = urbanDictionaryOpenTag + link + urbanDictionaryCloseTag;
-        } else if (name.equals("wikisaurus")) {
-            addDefinition = wikisaurusOpenTag + this.word + wikisaurusCloseTag;
+    private void initializeHtmlStrings(Context context) {
+        italicsTag = context.getResources().getString(R.string.italics_tag);
+        lineTag = context.getResources().getString(R.string.line_tag);
+        smallTag = context.getResources().getString(R.string.small_tag);
+        webLinkTag = context.getResources().getString(R.string.web_link_tag);
+        urbanDictionaryTag = context.getResources().getString(R.string.urban_dictionary_tag);
+        synonymTag = context.getResources().getString(R.string.synonym_tag);
+        noResultsTag = context.getResources().getString(R.string.no_results_tag);
+    }
+
+    public class Content
+    {
+        public String name;
+        public String content;
+        public Content(String name, String content) {
+            this.name = name;
+            this.content = content;
         }
-        return noResultsOpenTag + name + addDefinition + noResultsCloseTag;
     }
 
-    private String linkToWeb(Context c, String link) {
-        return c.getResources().getString(R.string.web_link_open_tag) + link +
-               c.getResources().getString(R.string.web_link_close_tag);
-    }
+    public enum Verb {
+        Dictionary, Synonym, Slang;
 
-    // getter methods
-    public String getDictionaryName(){ return dictionaryName; }
-    public String getSlangName(){ return slangName; }
-    public String getThesaurusName(){ return thesaurusName; }
-    public String getDictionaryContent(){ return dictionaryContent; }
-    public String getSlangContent(){ return slangContent; }
-    public String getThesaurusContent(){ return thesaurusContent; }
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase();
+        }
+    }
 }
--- app/src/main/java/com/mindsoon/lexplore/MyActivity.java
+++ app/src/main/java/com/mindsoon/lexplore/MyActivity.java
@@ -9,11 +9,11 @@ package com.mindsoon.lexplore;
         import android.os.Bundle;
         import android.text.Html;
         import android.text.method.LinkMovementMethod;
-        import android.util.Log;
         import android.view.KeyEvent;
         import android.view.View;
         import android.widget.EditText;
         import android.widget.LinearLayout;
+        import android.widget.RelativeLayout;
         import android.widget.ScrollView;
         import android.widget.TextView;
         import android.view.View.OnClickListener;
@@ -35,11 +35,13 @@ package com.mindsoon.lexplore;
         import java.util.ArrayList;
 
 public class MyActivity extends ActionBarActivity implements OnClickListener {
-    private LinearLayout layoutProgress, box_layout, splash_layout, about_layout;
-    private TextView dictionary_name, slang_name, thesaurus_name, dictionary_box, slang_box, thesaurus_box;
+    private LinearLayout box_layout, splash_layout, about_layout;
+    private RelativeLayout dictionary_spinner, slang_spinner, synonym_spinner;
+    private TextView dictionary_name, slang_name, synonym_name, dictionary_textview, slang_textview, synonym_textview;
     private EditText submitText;
     private ScrollView scroll_layout;
     private ArrayList<Entry> searchHistory = new ArrayList<Entry>();
+    private Entry entry;
     private boolean doubleBackToExitPressedOnce;
 
     @Override
@@ -49,7 +51,6 @@ public class MyActivity extends ActionBarActivity implements OnClickListener {
         getSupportActionBar().hide();
         toastIfNoInternet();
         setFields();
-
         submitText.setOnClickListener(this);
         submitText.setOnKeyListener(new View.OnKeyListener() {
             @Override
@@ -58,23 +59,94 @@ public class MyActivity extends ActionBarActivity implements OnClickListener {
                     return false;
                 } else if (!isNetworkAvailable()) {
                     toastIfNoInternet();
-                } else if ( (keyCode == KeyEvent.KEYCODE_BACK) && (searchHistory.size() > 1) ) {
+                } else if (keyCode == KeyEvent.KEYCODE_BACK && searchHistory.size() > 1) {
                     searchHistory.remove(searchHistory.size() - 1);
-                    submitText.setText(searchHistory.get(searchHistory.size() - 1).getWord());
+                    submitText.setText(searchHistory.get(searchHistory.size() - 1).word);
                     insertEntryIntoFields(searchHistory.get(searchHistory.size() - 1));
                     hideKeyboard(v);
-                } else if ( (keyCode == KeyEvent.KEYCODE_BACK) ||
-                        (keyCode == KeyEvent.KEYCODE_MENU) ) {
+                } else if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_MENU) {
                     checkForDoubleBackPress();
                     showSplash(v);
-                } else if ( (keyCode == KeyEvent.KEYCODE_ENTER) && (submitText.length() > 0) ) {
-                    Log.d("word", "getting definition of " + submitText.getText().toString());
-                    getDefinitions(v, submitText.getText().toString());
+                } else if (keyCode == KeyEvent.KEYCODE_ENTER && submitText.length() > 0) {
+                    entry = new Entry(submitText.getText().toString());
+                    if (!isWordValid(entry.word)) {
+                        showToast(getString(R.string.invalid_user_input_response));
+                        return true;
+                    }
                     hideKeyboard(v);
+                    hydrateEntryContent(v, Entry.Verb.Dictionary);
+                    hydrateEntryContent(v, Entry.Verb.Slang);
+                    hydrateEntryContent(v, Entry.Verb.Synonym);
                 }
                 return true;
             }
         });
+    }
+
+    boolean isWordValid(String word) {
+        return !word.equals("");
+    }
+
+    void hydrateEntryContent(final View v, final Entry.Verb verb){
+
+        class HttpGetAsyncTask extends AsyncTask<String, Void, JSONObject> {
+            @Override
+            protected void onPreExecute() {
+                showSpinner(verb);
+            }
+
+            @Override
+            protected JSONObject doInBackground(String... strings) {
+                int code = 0;
+                String url = getString(R.string.url) + verb + "/";
+                try {
+                    url += URLEncoder.encode(entry.word, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    url += entry.word.replaceAll("\\s+","");
+                }
+                JSONObject returnedData = new JSONObject();
+                try {
+                    HttpClient client = new DefaultHttpClient();
+                    final HttpParams httpParameters = client.getParams();
+                    HttpConnectionParams.setConnectionTimeout(httpParameters, 10000);
+                    HttpConnectionParams.setSoTimeout(httpParameters, 10000);
+                    HttpGet get = new HttpGet(url);
+                    HttpResponse responseGet = client.execute(get);
+                    HttpEntity resEntityGet = responseGet.getEntity();
+                    code = responseGet.getStatusLine().getStatusCode();
+                    if (resEntityGet != null) {
+                        String response = EntityUtils.toString(resEntityGet);
+                        returnedData = new JSONObject(response);
+                    }
+                } catch (SocketTimeoutException e) {
+                    showToast(getString(R.string.server_timeout_response));
+                } catch (IOException e) {
+                    showToast(getString(R.string.server_error_response));
+                } catch (Exception e) {
+                    showToast(code == 404
+                            ? getString(R.string.server_error_response)
+                            : getString(R.string.app_error_response));
+                }
+                return returnedData;
+            }
+
+            protected void onPostExecute(JSONObject jsonObject) {
+                hideSpinner(verb);
+                if (jsonObject.length() > 0)  {
+                    hideAllWidgets(false);
+                    entry.hydrateContentFromJson(jsonObject, verb, getBaseContext());
+                    if (entry.hasAnyContent()) {
+                        insertEntryIntoFields(entry);
+                        if (entry.hasAllContent()) {
+                            searchHistory.add(entry);
+                        }
+                    }
+                }
+            }
+        }
+
+        HttpGetAsyncTask aTask = new HttpGetAsyncTask();
+        aTask.execute(entry.word);
     }
 
     void showToast(final String toast) {
@@ -102,34 +174,42 @@ public class MyActivity extends ActionBarActivity implements OnClickListener {
     void setFields() {
         dictionary_name = (TextView) findViewById(R.id.dictionary_name);
         slang_name = (TextView) findViewById(R.id.slang_name);
-        thesaurus_name = (TextView) findViewById(R.id.thesaurus_name);
-        dictionary_box = (TextView) findViewById(R.id.dictionary_box);
-        slang_box = (TextView) findViewById(R.id.slang_box);
-        thesaurus_box = (TextView) findViewById(R.id.thesaurus_box);
+        synonym_name = (TextView) findViewById(R.id.synonym_name);
+        dictionary_textview = (TextView) findViewById(R.id.dictionary_textview);
+        slang_textview = (TextView) findViewById(R.id.slang_textview);
+        synonym_textview = (TextView) findViewById(R.id.synonym_textview);
         submitText = (EditText) findViewById(R.id.submitText);
-        layoutProgress = (LinearLayout) findViewById(R.id.progress_spinner_layout);
+        dictionary_spinner = (RelativeLayout) findViewById(R.id.dictionary_spinner);
+        slang_spinner = (RelativeLayout) findViewById(R.id.slang_spinner);
+        synonym_spinner = (RelativeLayout) findViewById(R.id.synonym_spinner);
         box_layout = (LinearLayout) findViewById(R.id.box_layout);
         splash_layout = (LinearLayout) findViewById(R.id.splash_layout);
         scroll_layout = (ScrollView) findViewById(R.id.scroll_layout);
         about_layout = (LinearLayout) findViewById(R.id.about_layout);
         TextView about_contact = (TextView) findViewById(R.id.about_contact);
-        dictionary_box.setMovementMethod(LinkMovementMethod.getInstance());
-        dictionary_box.setLinksClickable(true);
-        slang_box.setMovementMethod(LinkMovementMethod.getInstance());
-        slang_box.setLinksClickable(true);
-        thesaurus_box.setMovementMethod(LinkMovementMethod.getInstance());
-        thesaurus_box.setLinksClickable(true);
+        dictionary_textview.setMovementMethod(LinkMovementMethod.getInstance());
+        dictionary_textview.setLinksClickable(true);
+        slang_textview.setMovementMethod(LinkMovementMethod.getInstance());
+        slang_textview.setLinksClickable(true);
+        synonym_textview.setMovementMethod(LinkMovementMethod.getInstance());
+        synonym_textview.setLinksClickable(true);
         about_contact.setMovementMethod(LinkMovementMethod.getInstance());
         about_contact.setLinksClickable(true);
     }
 
     void insertEntryIntoFields(Entry entry) {
-        dictionary_name.setText(Html.fromHtml(entry.getDictionaryName()));
-        slang_name.setText(Html.fromHtml(entry.getSlangName()));
-        thesaurus_name.setText(Html.fromHtml(entry.getThesaurusName()));
-        dictionary_box.setText(Html.fromHtml(entry.getDictionaryContent()));
-        slang_box.setText(Html.fromHtml(entry.getSlangContent()));
-        thesaurus_box.setText(Html.fromHtml(entry.getThesaurusContent()));
+        if (entry.dictionary != null) {
+            dictionary_name.setText(Html.fromHtml(entry.dictionary.name));
+            dictionary_textview.setText(Html.fromHtml(entry.dictionary.content));
+        }
+        if (entry.synonym != null) {
+            synonym_name.setText(Html.fromHtml(entry.synonym.name));
+            synonym_textview.setText(Html.fromHtml(entry.synonym.content));
+        }
+        if (entry.slang != null) {
+            slang_name.setText(Html.fromHtml(entry.slang.name));
+            slang_textview.setText(Html.fromHtml(entry.slang.content));
+        }
         scroll_layout.scrollTo(0, 0);
     }
 
@@ -157,85 +237,46 @@ public class MyActivity extends ActionBarActivity implements OnClickListener {
         imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
 
-    public void hideAllWidgets(){
+    public void hideAllWidgets(boolean hideBox){
+        box_layout.setVisibility(hideBox ? View.GONE : View.VISIBLE);
         about_layout.setVisibility(View.GONE);
-        box_layout.setVisibility(View.GONE);
-        layoutProgress.setVisibility(View.GONE);
         splash_layout.setVisibility(View.GONE);
     }
 
     public void showAbout(View v) {
-        hideAllWidgets();
+        hideAllWidgets(true);
         about_layout.setVisibility(View.VISIBLE);
     }
 
-    void showResults(View v) {
-        hideAllWidgets();
-        box_layout.setVisibility(View.VISIBLE);
+    void showSpinner(Entry.Verb verb) {
+        hideAllWidgets(false);
+        if (verb == Entry.Verb.Dictionary) {
+            dictionary_spinner.setVisibility(View.VISIBLE);
+            dictionary_textview.setVisibility(View.GONE);
+        } else if (verb == Entry.Verb.Slang) {
+            slang_spinner.setVisibility(View.VISIBLE);
+            slang_textview.setVisibility(View.GONE);
+        } else if (verb == Entry.Verb.Synonym) {
+            synonym_spinner.setVisibility(View.VISIBLE);
+            synonym_textview.setVisibility(View.GONE);
+        }
     }
 
-    void showSpinner(View v) {
-        hideAllWidgets();
-        layoutProgress.setVisibility(View.VISIBLE);
+    void hideSpinner(Entry.Verb verb) {
+        if (verb == Entry.Verb.Dictionary) {
+            dictionary_spinner.setVisibility(View.GONE);
+            dictionary_textview.setVisibility(View.VISIBLE);
+        } else if (verb == Entry.Verb.Slang) {
+            slang_spinner.setVisibility(View.GONE);
+            slang_textview.setVisibility(View.VISIBLE);
+        } else if (verb == Entry.Verb.Synonym) {
+            synonym_spinner.setVisibility(View.GONE);
+            synonym_textview.setVisibility(View.VISIBLE);
+        }
     }
 
     public void showSplash(View v) {
-        hideAllWidgets();
+        hideAllWidgets(true);
         splash_layout.setVisibility(View.VISIBLE);
-    }
-
-    void getDefinitions(final View v, final String submittedWord){
-
-        class HttpGetAsyncTask extends AsyncTask<String, Void, JSONObject> {
-
-            @Override
-            protected void onPreExecute() {
-                showSpinner(v);
-            }
-
-            @Override
-            protected JSONObject doInBackground(String... strings) {
-                String url;
-                try {
-                    url = getString(R.string.url) + URLEncoder.encode(submittedWord, "UTF-8");
-                } catch (UnsupportedEncodingException e) {
-                    url = getString(R.string.url) + submittedWord.replaceAll("\\s+","");
-                }
-                JSONObject returnedData = new JSONObject();
-                try {
-                    HttpClient client = new DefaultHttpClient();
-                    final HttpParams httpParameters = client.getParams();
-                    HttpConnectionParams.setConnectionTimeout(httpParameters, 10000);
-                    HttpConnectionParams.setSoTimeout        (httpParameters, 10000);
-                    HttpGet get = new HttpGet(url);
-                    HttpResponse responseGet = client.execute(get);
-                    HttpEntity resEntityGet = responseGet.getEntity();
-                    if (resEntityGet != null) {
-                        String response = EntityUtils.toString(resEntityGet);
-                        returnedData = new JSONObject(response);
-                    }
-                } catch (SocketTimeoutException e) {
-                    showToast(getString(R.string.no_server_response));
-                } catch (IOException e) {
-                    showToast(getString(R.string.no_server_response));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return returnedData;
-            }
-
-            protected void onPostExecute(JSONObject definitionObject) {
-                showResults(v);
-                Entry thisEntry = new Entry(submittedWord);
-                thisEntry.insertContent(definitionObject, getBaseContext());
-                insertEntryIntoFields(thisEntry);
-                searchHistory.add(thisEntry);
-                Log.d("word", searchHistory.get(searchHistory.size()-1).word +
-                        " added to history of " + searchHistory.size() + " words");
-            }
-        }
-
-        HttpGetAsyncTask aTask = new HttpGetAsyncTask();
-        aTask.execute(submittedWord);
     }
 }
